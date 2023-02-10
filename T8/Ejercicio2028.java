f065b9d4a21dbf856a47ebfe4c6d80e9da3125f0
rasial27 <rsilalc465@g.educaand.es>
2023-02-10 18:31:25 +0100

    rsa

--- T8/Ejercicio2028.java
+++ T8/Ejercicio2028.java
@@ -1,5 +1,6 @@
 package T8;
 import java.util.Scanner;
+import T8.misFunciones.FunciArray;
 /*
  * Ejercicio2028.java
  * Crea las funciones de los ejercicios 20 al 28
@@ -8,48 +9,68 @@ import java.util.Scanner;
  
  */
 public class Ejercicio2028 {
-
-  public static int[] generaArrayInt(int n, int min, int max) {
-   
-    int[] num = new int[n];
-
-    for( int i = 0; i < n; i++) {
-      num[i] = (int)(Math.random()*(max+1-min)) +min;
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n, min, max, num;
+    int[] array;
+          
+    System.out.print("Introduce el tamaño del array: ");
+    n = sc.nextInt();
+          
+    System.out.print("Introduce el mínimo valor para los números aleatorios: ");
+    min = sc.nextInt();
+          
+    System.out.print("Introduce el máximo valor para los números aleatorios: ");
+    max = sc.nextInt();
+          
+    array = FunciArray.generaArrayInt(n, min, max);
+    System.out.print("El array generado es: ");
+    for(int i = 0; i < array.length; i++) {
+      System.out.print(array[i] + " ");
     }
-    return num;
-
     
-  }
-
-  public static int minimoArrayInt(int[] x) {
-    int min = Integer.MAX_VALUE;
-    
-    for(int i = 0; i < x.length; i++){
-      if (x[i] < min) {
-        min = x[i];
-      }
+          
+    System.out.println("\nEl mínimo valor del array es: " + FunciArray.minimoArrayInt(array));
+    System.out.println("El máximo valor del array es: " + FunciArray.maximoArrayInt(array));
+    System.out.println("La media del array es: " + FunciArray.mediaArray(array));
+          
+    System.out.print("Introduce un número a buscar en el array: ");
+    num = sc.nextInt();
+    if (FunciArray.estaEnArrayInt(array, num)) {
+      System.out.println("El número está en el array en la posición: " + FunciArray.posicionEnArray(array, num));
+      } else {
+        System.out.println("El número no está en el array");
+        }
+          
+  System.out.println("\nArray original: ");
+    for(int i = 0; i < array.length; i++) {
+      System.out.print(array[i] + " ");
     }
-    return min;
-  }
-
-  public static int maximoArrayInt(int[] x) {
-    int max = Integer.MIN_VALUE;
-    
-    for(int i = 0; i < x.length; i++) {
-    if (x[i] < max) {
-      max = x[i];
-      }
+  FunciArray.volteaArrayInt(array);
+  System.out.println("\nArray volteado: ");
+    for(int i = 0; i < array.length; i++) {
+      System.out.print(array[i] + " ");
     }
-      
-    return max;
+       
+  System.out.print("\nIntroduce el número de posiciones a rotar a la derecha: ");
+  int posDerecha = sc.nextInt();
+  FunciArray.rotaDerechaArrayInt(array, posDerecha);
+  System.out.print("\nEl array generado es: ");
+    for(int i = 0; i < array.length; i++) {
+      System.out.print(array[i] + " ");
+    }
+          
+  System.out.print("\nIntroduce el número de posiciones a rotar a la izquierda: ");
+  int posIzquierda = sc.nextInt();
+  FunciArray.rotaIzquierdaArrayInt(array, posIzquierda);
+  System.out.print("\nEl array generado es: ");
+    for(int i = 0; i < array.length; i++) {
+      System.out.print(array[i] + " ");
+    }
+  
   }
-
-  public static double
-
-
-
-
 }
+  
 
 
   
--- T8/misFunciones/FunciArray.java
+++ T8/misFunciones/FunciArray.java
@@ -14,5 +14,83 @@ public class FunciArray {
     return (double)sumaArray(array)/array.length;
   }
 
+  public static int[] generaArrayInt(int n, int minimo, int maximo) {
+    int[] array = new int[n];
+    for (int i = 0; i < array.length; i++) {
+       array[i] = (int) (Math.random() * (maximo - minimo + 1) + minimo);
+    }
+    return array;
+  }
+
+  public static int minimoArrayInt(int[] x) {
+    int min = Integer.MAX_VALUE;
+  
+    for(int i = 0; i < x.length; i++){
+      if (x[i] < min) {
+        min = x[i];
+      }
+    }
+    return min;
+  }
+
+  public static int maximoArrayInt(int[] x) {
+    int max = Integer.MIN_VALUE;
+    
+    for(int i = 0; i < x.length; i++) {
+    if (x[i] > max) {
+      max = x[i];
+      }
+    }
+      
+    return max;
+  }
+
+  public static boolean estaEnArrayInt(int[] array, int numero) {
+    for (int i = 0; i < array.length; i++) {
+       if (array[i] == numero) {
+          return true;
+       }
+    }
+    return false;
+  }
+
+  public static int posicionEnArray(int[] array, int numero) {
+    for (int i = 0; i < array.length; i++) {
+       if (array[i] == numero) {
+          return i;
+       }
+    }
+    return -1;
+  }
+
+  public static void volteaArrayInt(int[] array) {
+    int aux;
+    for (int i = 0; i < array.length / 2; i++) {
+       aux = array[i];
+       array[i] = array[array.length - i - 1];
+       array[array.length - i - 1] = aux;
+    }
+  }
+
+  public static void rotaDerechaArrayInt(int[] array, int posiciones) {
+    int aux = array[array.length - 1];
+    for (int i = array.length - 1; i > 0; i--) {
+       array[i] = array[i - 1];
+    }
+    array[0] = aux;
+    if (posiciones > 1) {
+       rotaDerechaArrayInt(array, posiciones - 1);
+    }
+  }
+
+  public static int[] rotaIzquierdaArrayInt(int[] array, int n) {
+    int[] result = new int[array.length];
+
+    for (int i = 0; i < array.length; i++) {
+        result[(i + array.length - n) % array.length] = array[i];
+    }
+    return result;
+  }
+
   
 }
