8424df625f86e66b91e3864b2496344a74ed3aea
rasial27 <rsilalc465@g.educaand.es>
2023-03-09 18:40:27 +0100

    rsa

---
+++ T9/Ejercicio8/Terminal.java
@@ -0,0 +1,30 @@
+package T9.Ejercicio8;
+
+public class Terminal {
+  
+  private final String numero;
+  private int tiempoConversaciones;
+
+  public Terminal(String numero) {
+    this.numero = numero;
+    this.tiempoConversaciones = 0;
+  }
+
+  public String getNumero() {
+    return numero;
+  }
+
+  public int getTiempoConversaciones() {
+    return tiempoConversaciones;
+  }
+
+  public void llama(Terminal terminal, int tiempo) {
+    this.tiempoConversaciones += tiempo;
+    terminal.tiempoConversaciones += tiempo;
+  }
+
+  @Override
+  public String toString() {
+    return "Nº " + numero + " - " + tiempoConversaciones + " s de conversacion";
+  }
+}
