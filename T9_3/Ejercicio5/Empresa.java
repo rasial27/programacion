d5a2572d7bf0b687ecd2b427bc073936254397a0
rasial27 <rsilalc465@g.educaand.es>
2023-03-26 13:08:35 +0200

    rsa

--- T9_3/Ejercicio5/Empresa.java
+++ T9_3/Ejercicio5/Empresa.java
@@ -7,81 +7,79 @@ class Empresa {
     private String direccion;
     private Empleado[] empleados;
     private int cantidadEmpleados;
-
+  
     public Empresa(String nombre, String cif) {
-        this.nombre = nombre;
-        this.cif = cif;
-        this.empleados = new Empleado[100];
+      this.nombre = nombre;
+      this.cif = cif;
+      this.empleados = new Empleado[100];
     }
-
+  
     public String getNombre() {
-        return nombre;
+      return nombre;
     }
-
+  
     public String getCif() {
-        return cif;
+      return cif;
     }
-
+  
     public String getTelefono() {
-        return telefono;
+      return telefono;
     }
-
+  
     public void setTelefono(String telefono) {
-        this.telefono = telefono;
+      this.telefono = telefono;
     }
-
+  
     public String getDireccion() {
-        return direccion;
+      return direccion;
     }
-
+  
     public void setDireccion(String direccion) {
-        this.direccion = direccion;
+      this.direccion = direccion;
     }
-
+  
     public void anadirEmpleado(Empleado empleado) {
-        empleados[cantidadEmpleados] = empleado;
-        cantidadEmpleados++;
+      empleados[cantidadEmpleados] = empleado;
+      cantidadEmpleados++;
     }
-
+  
     public void eliminarEmpleado(String dni) {
-        for (int i = 0; i < cantidadEmpleados; i++) {
-            if (empleados[i].getDni().equals(dni)) {
-                for (int j = i; j < cantidadEmpleados - 1; j++) {
-                    empleados[j] = empleados[j + 1];
-                }
-                cantidadEmpleados--;
-                return;
-            }
+      for (int i = 0; i < cantidadEmpleados; i++) {
+        if (empleados[i].getDni().equals(dni)) {
+          for (int j = i; j < cantidadEmpleados - 1; j++) {
+            empleados[j] = empleados[j + 1];
+          }
+          cantidadEmpleados--;
+          return;
         }
+      }
     }
-
+  
     public void imprimirEmpleados() {
-        for (int i = 0; i < cantidadEmpleados; i++) {
-            empleados[i].imprimirInformacion();
-            System.out.println("----------------------");
-        }
+      for (int i = 0; i < cantidadEmpleados; i++) {
+        empleados[i].imprimirInformacion();
+        System.out.println("----------------------");
+      }
     }
-
+  
     public double calcularSueldos() {
-        double sueldos = 0;
-        for (int i = 0; i < cantidadEmpleados; i++) {
-            sueldos += empleados[i].calcularSueldoNeto();
-        }
-        return sueldos;
+      double sueldos = 0;
+      for (int i = 0; i < cantidadEmpleados; i++) {
+        sueldos += empleados[i].calcularSueldoNeto();
+      }
+      return sueldos;
     }
-
+  
     public int getCantidadEmpleados() {
-        return cantidadEmpleados;
+      return cantidadEmpleados;
     }
     
     public Empleado[] obtenerEmpleados() {
-        Empleado[] listaEmpleados = new Empleado[cantidadEmpleados];
-        for (int i = 0; i < cantidadEmpleados; i++) {
-            listaEmpleados[i] = empleados[i];
-        }
-        return listaEmpleados;
+      Empleado[] listaEmpleados = new Empleado[cantidadEmpleados];
+      for (int i = 0; i < cantidadEmpleados; i++) {
+        listaEmpleados[i] = empleados[i];
+      }
+      return listaEmpleados;
     }
-    
-}
-
+  }
 
